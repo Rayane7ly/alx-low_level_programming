db69b3e660d7321dc78201d5111911e9fbe85e18
Rayanex7 <lyachaarayan@gmail.com>
2024-01-05 23:47:52 +0100

    fifth project

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,15 @@
 #include<stdio.h>
-int main(void){
-	puts("\"Programming is like building a multilingual puzzle\n");
-return 0;
+
+/**
+ * main - Entry point
+ *
+ * Description: A C programme that prints with puts function
+ *
+ * return: Always 0 (success)
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
