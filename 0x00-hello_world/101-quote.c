ef27024bf9e77955935ce97d76200a81aaf510f1
Rayanex7 <lyachaarayan@gmail.com>
2024-01-09 19:40:57 +0100

    Final task

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,24 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+*/
+
+void ft_putchar(char c){
+	write(1, &c, 1);
+}
+
+void ft_putstr(char *str){
+	char *ptr = str;
+	while(*ptr != '\0'){
+		ft_putchar(*ptr);
+		ptr++;
+	}
+}
+
+int main(){
+	ft_putstr("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return 1;
+}
