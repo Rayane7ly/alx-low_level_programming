202736295eaac071a29ab9c0e947c3ba6d9268e2
Rayanex7 <lyachaarayan@gmail.com>
2024-01-09 19:42:56 +0100

    Final task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,19 +6,23 @@
  * Return: Always 0 (success)
 */
 
-void ft_putchar(char c){
+void ft_putchar(char c)
+{
 	write(1, &c, 1);
 }
 
-void ft_putstr(char *str){
+void ft_putstr(char *str)
+{
 	char *ptr = str;
-	while(*ptr != '\0'){
+	while(*ptr != '\0')
+{
 		ft_putchar(*ptr);
 		ptr++;
-	}
+}
 }
 
-int main(){
+int main()
+{
 	ft_putstr("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return 1;
 }
