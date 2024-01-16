7d212eae51dd6472be67669e54cc6587a263506f
Rayanex7 <lyachaarayan@gmail.com>
2024-01-16 21:38:15 +0100

    Adding project 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints numbers starting from 0 to 10.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n = 0;
+
+	while (n <= 10)
+	{
+		printf("%d", n);
+		n++;
+	}
+
+	printf("\n");
+
+	return (0);
+}
