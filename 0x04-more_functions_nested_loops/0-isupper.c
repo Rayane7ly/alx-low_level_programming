c388ac9b68b145ff8997eef2b6c26fbc73039c0e
Rayanex7 <lyachaarayan@gmail.com>
2024-04-16 12:46:19 +0100

    Uppercase or lowercase

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,14 +1,20 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
 
-int _isupper(int c){
+/**
+ * _isupper - check if c is uppercase or lowercase
+ * @c: the character to be checked
+ *
+ * Return: 1 if uppercase, 0 otherwise
+ */
+int _isupper(int c)
+{
 	if (c >= 65 && c <= 90)
 	{
-		return 1;
+		return (1);
 	}
-	else 
+	else
 	{
-		return 0;
+		return (0);
 	}
-
 }
