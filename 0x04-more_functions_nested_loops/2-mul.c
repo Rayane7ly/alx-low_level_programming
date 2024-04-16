494d417799740f89845de400d61f97d1865de100
Rayanex7 <lyachaarayan@gmail.com>
2024-04-16 23:55:30 +0100

    sum of two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * mul - calculate sum of two integers
+ *
+ * @a: variable to be multiplied
+ * @b: variable to be multiplied
+ *
+ * Return: Return a * b
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
