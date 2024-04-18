ac131920d4642c010b53e9e2e28b47eeb926e52b
Rayanex7 <lyachaarayan@gmail.com>
2024-04-18 11:56:02 +0100

    print numbers from 1 to 14

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/***/
+void more_numbers(void)
+{
+	int row,num,count;
+
+	for (row = 0; row < 10; row++)
+	{
+		for (count = 0 ; count <= 14 ; count++)
+		{
+			num = count;
+			if (count > 9)
+			{
+				_putchar(1 + 48);
+				num = count % 10;
+			}
+			_putchar(num + 48);
+		}
+		_putchar('\n');
+	}
+}
