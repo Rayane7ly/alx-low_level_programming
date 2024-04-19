f5254c58a82234235c8d69dbc5fe9fa881f4e944
Rayanex7 <lyachaarayan@gmail.com>
2024-04-19 14:17:34 +0100

    print diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_diagonal - print a diagonal with '\'
+ *
+ * @n: variable to be used
+*/
+void print_diagonal(int n)
+{
+	int i;
+	int j;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 1 ; i <= n ; i++)
+		{
+			for (j = 1 ; j <= i ; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+}
