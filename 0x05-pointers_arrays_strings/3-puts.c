6b49eb2933b0d45a752da2d1e514f4212cad9dae
Rayanex7 <lyachaarayan@gmail.com>
2024-04-24 21:21:29 +0100

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * _puts - functions to print
+ *
+ * @str: variable pointer which contains the writting
+*/
+void _puts(char *str)
+{
+	char *ptr = str;
+
+	while (*ptr != '\0')
+	{
+		write(1, ptr, 1);
+		ptr++;
+	}
+	write(1, "\n", 1);
+}
