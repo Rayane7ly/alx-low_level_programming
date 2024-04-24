1fb3d1c872ba8b81ceeefcd150572d41bd225990
Rayanex7 <lyachaarayan@gmail.com>
2024-04-24 12:42:11 +0100

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strlen - return lenght of a string
+ *
+ * @s: variable lenght to be calculated
+ *
+ * Return: return "a" the lenght of s
+*/
+int _strlen(char *s)
+{
+	int a;
+
+	a = strlen(s);
+	return (a);
+}
