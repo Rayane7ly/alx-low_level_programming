3727760ce8c64546a049d71516d5bf694394dd25
Rayanex7 <lyachaarayan@gmail.com>
2024-06-29 20:07:04 +0100

    seventh project

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * void puts_half - print half of a string
+ * puts_half - print half of a string
  * @str: the variable to stock characters
 */
 
@@ -9,37 +9,33 @@ void puts_half(char *str)
 {
 	int i = 0;
 	int a;
-	int l,n;
+	int l, n;
 
 	while (str[i] != '\0')
 		i++;
 	l = i;
-	
+
 	a = i / 2;
-	
+
 	if (i % 2 == 0)
 	{
 		i = 0;
 
 		while (str[i] != '\0')
 		{
-			putchar(str[i+a]);
+			putchar(str[i + a]);
 			i++;
 		}
 		putchar('\n');
 	}
 
 	else
-        {
-		n = (l - 1 ) / 2;
-
-                for (i = l - n ; i < l ; i++)
+	{
+		n = (l - 1) / 2;
+		for (i = l - n ; i < l ; i++)
 		{
 			putchar(str[i]);
-                }
+		}
 		putchar('\n');
-
-        }
-
-
+	}
 }
